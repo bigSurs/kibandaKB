33348b53af2008dcd42ff54887d04ec7590811a3
Victor Kwikbasket <96406967+bigSurs@users.noreply.github.com>
2022-08-02 11:31:01 +0300

    delivery_location_cubits

--- lib/cubits/vendor_products/vendor_products_cubit.dart
+++ lib/cubits/vendor_products/vendor_products_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:kibanda_kb/models/kibanda_model/kibanda.dart';
-import 'package:kibanda_kb/models/vendor_prodcuts/vendor_products.dart';
+
 import 'package:kibanda_kb/services/api_service/api_service.dart';
 
 import 'package:kibanda_kb/utilities/rest_client/rest_client.dart';
@@ -12,10 +11,7 @@ part 'vendor_products_cubit.freezed.dart';
 class VendorProductsCubit extends Cubit<VendorProductsState> {
   VendorProductsCubit() : super(VendorProductsState.initial());
 
-  getVendorProductsByAllCategories(
-      {
-      required int customerId
-      }) async {
+  getVendorProductsByAllCategories({required int customerId}) async {
     emit(const VendorProductsState.loading());
     try {
       await ApiService.post(data: {
@@ -29,5 +25,3 @@ class VendorProductsCubit extends Cubit<VendorProductsState> {
     }
   }
 }
-
-
